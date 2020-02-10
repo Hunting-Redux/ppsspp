a88757aca89231f4d9361586b451e233f8239d66
EclipseKnight <32472185+EclipseKnight@users.noreply.github.com>
2020-02-09 21:47:35 -0500

    Add files via upload

---
+++ git-version.cpp
@@ -0,0 +1,6 @@
+// This is a generated file, by git-version-gen.cmd. 
+ 
+const char *PPSSPP_GIT_VERSION = "v1.9.3-Hunting-Redux"; 
+ 
+// If you don't want this file to update/recompile, change to 1. 
+#define PPSSPP_GIT_VERSION_NO_UPDATE 1 
